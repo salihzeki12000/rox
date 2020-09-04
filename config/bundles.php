9a82332cb16b5a3a4a891f177488271392b3a8db
Vincent Chalamon <vincentchalamon@gmail.com>
2020-09-04 11:44:12 +0200

    Install & configure lexik/jwt-authentication-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -21,4 +21,5 @@ return [
     BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
